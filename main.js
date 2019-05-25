63038bfd3dccaa8882281c5f05d2c63f90170acf
Jamar Powell <twoyearsyoung@gmail.com>
2019-05-24 20:36:00 -0700

    Final Update for the night
    Added
    - MethodOverride
    - Username to campgroundSchema
    - Updated css
    - Added Auth to several routes.
    - Created Edit/Update routes.
    - Created Edit form.
    -Added edit button to Show page.

--- main.js
+++ main.js
@@ -5,13 +5,15 @@ const express = require('express');
 const port = 3000;
 const app = express();
 
+const methodOverride = require('method-override');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const LocalStrategy = require('passport-local');
 const mongoose = require('mongoose');
 const User = require('./models/users');
-const Campground = require('./models/campgrounds');
-const Comment = require('./models/comments');
+
+// const Campground = require('./models/campgrounds');
+// const Comment = require('./models/comments');
 // const seedDB = require('./seeds');
 
 // ROUTES
@@ -42,6 +44,7 @@ mongoose.connect('mongodb://localhost:27017/su_camp', {
   useNewUrlParser: true,
 });
 app.use(express.static(`${__dirname}/public`));
+app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('view engine', 'ejs');
 
--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -1,8 +1,18 @@
+/* eslint-disable no-underscore-dangle */
 /* eslint-disable no-console */
 const express = require('express');
 const Campground = require('../models/campgrounds');
 
 const router = express.Router();
+
+// Middleware
+
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) {
+    return next() || null; // REMEMBER TO ALWAYS INVOKE NEXT ON RETURN OR NOTHING HAPPENS.
+  }
+  res.redirect('/login');
+}
 
 // INDEX ROUTE
 router.get('/', (req, res) => {
@@ -16,29 +26,34 @@ router.get('/', (req, res) => {
 }); //
 
 // CREATE ROUTE
-router.post('/', (req, res) => {
+router.post('/', isLoggedIn, (req, res) => {
   const { name } = req.body;
   const { image } = req.body;
   const desc = req.body.description;
+  const author = {
+    id: req.user._id,
+    username: req.user.username,
+  };
+
 
   const newCamp = {
     name,
     image,
     description: desc,
+    author,
   };
 
   Campground.create(newCamp, (err) => {
     if (err) {
-      console.log(err);
-    } else {
-      res.redirect('/campgrounds'); // refresh page back to campgrounds.
+      return console.error();
     }
+    res.redirect('/campgrounds'); // refresh page back to campgrounds.
   });
 
   // campSites.push(newCamp); // Add new camp to list.
 });
 
-router.get('/new', (req, res) => {
+router.get('/new', isLoggedIn, (req, res) => {
   res.render('campgrounds/new');
 });
 
@@ -54,6 +69,28 @@ router.get('/:id', (req, res) => {
         res.render('campgrounds/show', { campground: found });
       }
     });
+});
+
+// Edit Campground
+router.get('/:id/edit', (req, res) => {
+  Campground.findById(req.params.id)
+    .exec((err, found) => {
+      if (err) {
+        return new Error();
+      }
+      console.log(found);
+      res.render('campgrounds/edit', { campground: found });
+    });
+});
+// Update Campground
+router.put('/:id', (req, res) => {
+  Campground.findByIdAndUpdate(req.params.id, req.body.campground, (err, updatedCamp) => {
+    if (err) {
+      console.log(err, updatedCamp);
+    } else {
+      res.redirect(`/campgrounds/${req.params.id}`);
+    }
+  });
 });
 
 module.exports = router;
